252e9b773431916340f3cb72bf35005a36d3ca88
quyet.lm <quyet.lm@teko.vn>
2023-12-14 16:06:57 +0700

    clean vc

---
+++ vocab/clean_vocab.py
@@ -0,0 +1,18 @@
+l = []
+s = set()
+
+f_name = "vi_vocab"
+
+with open(f_name + ".txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        if line in s:
+            continue
+        else:
+            s.add(line)
+            l.append(line)
+
+with open(f_name + "_cleaned" + ".txt", "w") as f:
+    f.writelines(l)
+
+
